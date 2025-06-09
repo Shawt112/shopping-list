84dc5e6d9f69508690c971525f6f30876198a1a1
Shawt112 <tom_shaw@live.co.uk>
2025-06-09 21:06:34 +0100

    Update home.py

--- home.py
+++ home.py
@@ -10,9 +10,6 @@ st.markdown("---")
 st.header("🚀 Quick Links")
 
 # Use Streamlit's built-in page navigation links
-st.page_link("pages/About.py", label="📖 About")
-st.page_link("pages/Projects.py", label="🛠️ Projects")
-st.page_link("pages/Contact.py", label="📬 Contact")
 st.page_link("pages/Meal Planner.py", label="🗓️ Meal Planner")
 st.page_link("pages/Recipes.py", label="🍽️ Recipes")
 st.page_link("pages/Shopping List.py", label="🛒 Shopping List")
